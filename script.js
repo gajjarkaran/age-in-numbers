dd41fe6ca94397fa8787dd370c9958f3859cdfcb
Karan Gajjar <karangajjar96@gmail.com>
2021-07-30 11:57:19 -0400

    finished challenge

---
+++ script.js
@@ -0,0 +1,16 @@
+// your age in days
+
+function ageInDays(){
+  var birthYear = prompt ("What year were you born in?");
+  var ageInDayss = (2021 - birthYear) * 365;
+  // console.log(ageInDays);
+  var h1 = document.createElement('h1');
+  var textAnswer = document.createTextNode(`You are ${ageInDayss} days old.`);
+  h1.setAttribute('id', 'ageInDays');
+  h1.appendChild(textAnswer);
+  document.getElementById('flex-box-result').appendChild(h1);
+}
+
+function reset(){
+  document.getElementById('ageInDays').remove();
+}
